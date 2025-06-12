6bcd52c47216f833c44353210393f435e7e0b2c1
Madhusree-2005 <madhusreesandiri435@gmail.com>
2025-06-12 06:52:36 +0000

    vmshi

---
+++ function.js
@@ -0,0 +1,45 @@
+//Functions are one of the most powerful features in javascript.
+//
+//
+// they allow you to reuse code,organize logic,and handle dynamic inputs
+
+//
+// declaration +call
+
+//syntax
+// Function declaration
+
+sayHello()
+
+function sayHello(){
+    console.log("Hello")
+}
+sayHello()
+// this is known as jshosting
+
+// Function Calling
+sayHello()
+sayHello()
+
+let a=10;
+console.log(a)
+
+//functions with parameter
+function Sum( num1,num2,num3){
+    console.log(num1+num2+num3)
+}
+Sum(10,5,5)
+Sum(10,10,10)
+function Sub( num1,num2,num3){
+    console.log(num1-num2-num3)
+}
+
+Sub(10,5,5)
+Sub(10,10,10)
+
+function mul( num1,num2,num3){
+    console.log(num1*num2*num3)
+}
+
+mul(10,5,5)
+mul(10,10,10)
